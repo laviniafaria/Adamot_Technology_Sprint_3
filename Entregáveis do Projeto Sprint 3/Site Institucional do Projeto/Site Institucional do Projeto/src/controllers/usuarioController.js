c0b093d5dcf7f035f543389f4663018de5c87311
SammRibeiro <samuel.florenco@sptech.school>
2025-11-29 13:36:32 -0300

    fix: Correção da verificação do token

--- Entregáveis do Projeto Sprint 3/Site Institucional do Projeto/Site Institucional do Projeto/src/controllers/usuarioController.js
+++ Entregáveis do Projeto Sprint 3/Site Institucional do Projeto/Site Institucional do Projeto/src/controllers/usuarioController.js
@@ -47,20 +47,23 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    var fkShopping = req.body.idEmpresaVincularServer;
 
 
     // Faça as validações dos valores
+     // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    }
-    else {
+    } else if (fkShopping == undefined) {
+        res.status(400).send("Sua empresa a vincular está undefined!");
+    } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome, email, senha, fkShopping)
             .then(
                 function (resultado) {
                     res.json(resultado);
