1e0a96fe9b78884b8653355a0618906f7e9b265e
D-Foschini <daniel.foschini@sptech.school>
2025-12-08 03:21:00 -0300

    Fixes Dashboard com banco desalinhados (certifiquem que todos estão usando a modelagem do script_tabelas.sql)

--- Entregáveis do Projeto Sprint 3/Site Institucional do Projeto/Site Institucional do Projeto/src/models/registroModel.js
+++ Entregáveis do Projeto Sprint 3/Site Institucional do Projeto/Site Institucional do Projeto/src/models/registroModel.js
@@ -1,77 +1,73 @@
 var database = require("../database/config");
 
 function buscarUltimosRegistros(fkShopping, limite_linhas) {
-
-    var instrucaoSql = `SELECT 
+  var instrucaoSql = `SELECT 
                     DATE_FORMAT(r.dtHora, '%H:%i') AS hora,
                     SUM(r.valor) AS pessoas
                     FROM registro r
                     JOIN sensor s ON r.fkSensor = s.idSensor
-                    WHERE s.shopping_id = ${fkShopping}
+                    WHERE s.fkEntrada = ${fkShopping}
                     GROUP BY  DATE_FORMAT(r.dtHora, '%H:%i')
                     ORDER BY hora DESC LIMIT ${limite_linhas}`;
 
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
 }
 
 function buscarRegistrosEmTempoReal(fkShopping) {
-
-    var instrucaoSql = `SELECT 
+  var instrucaoSql = `SELECT 
                         SUM(r.valor) as pessoas,
                         DATE_FORMAT(r.dtHora,'%H:%i') as momento_grafico 
                         FROM registro r
                         JOIN sensor s ON r.fkSensor = s.idSensor 
-                        WHERE s.shopping_id = ${fkShopping}
+                        WHERE s.fkEntrada = ${fkShopping}
                         GROUP BY r.dtHora
                     ORDER BY r.dtHora DESC LIMIT 1`;
 
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
 }
 
 function buscarRegistroSensorA(fkShopping, limite_linhas) {
-
-    var instrucaoSql = `SELECT 
+  var instrucaoSql = `SELECT 
                     DATE_FORMAT(r.dtHora, '%H:%i') AS hora,
                     SUM(r.valor) AS pessoas
                     FROM registro r
                     JOIN sensor s ON r.fkSensor = s.idSensor
-                    WHERE s.shopping_id = ${fkShopping}
+                    WHERE s.idShopping = ${fkShopping}
                     GROUP BY  DATE_FORMAT(r.dtHora, '%H:%i')
                     ORDER BY hora DESC LIMIT ${limite_linhas}`;
 
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
 }
 
 function totalVisitantes(fkShopping) {
+  var instrucaoSql = `select * from vw_total_visitantes where idShopping = ${fkShopping}`;
 
-    var instrucaoSql = `select * from vw_total_visitantes where shopping_id = ${fkShopping}`;
-
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
 }
 
 function picoVisitantes(fkShopping) {
-  var instrucaoSql = `select * from vw_pico where shopping_id = ${fkShopping} limit 1`;
+  var instrucaoSql = `select * from vw_pico where idShopping = ${fkShopping} limit 1`;
 
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);  
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
 }
 
 function mediaVisitantes(fkShopping) {
-  var instrucaoSql = `select * from vw_media where shopping_id = ${fkShopping} limit 1;`;
+  var instrucaoSql = `select * from vw_media where idShopping = ${fkShopping} limit 1;`;
 
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);  
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
 }
 
 module.exports = {
-    buscarUltimosRegistros,
-    buscarRegistrosEmTempoReal,
-    buscarRegistroSensorA,
-    totalVisitantes,
-    picoVisitantes,
-    mediaVisitantes
-}
+  buscarUltimosRegistros,
+  buscarRegistrosEmTempoReal,
+  buscarRegistroSensorA,
+  totalVisitantes,
+  picoVisitantes,
+  mediaVisitantes,
+};
