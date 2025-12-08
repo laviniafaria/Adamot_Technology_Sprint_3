b6ed2744bbf18c8ca28f00aa82be99622cb72488
João Pedro <joao.coimbra@sptech.school>
2025-12-08 00:17:02 -0300

    add: bobIA modified: kpis front-end

--- Entregáveis do Projeto Sprint 3/Site Institucional do Projeto/Site Institucional do Projeto/src/routes/registros.js
+++ Entregáveis do Projeto Sprint 3/Site Institucional do Projeto/Site Institucional do Projeto/src/routes/registros.js
@@ -9,18 +9,18 @@ router.get("/ultimos/:fkShopping", function (req, res) {
 
 router.get("/tempo-real/:fkShopping", function (req, res) {
     registroController.buscarRegistrosEmTempoReal(req, res);
-})
+});
 
   router.get("/totalVisitantes/:fkShopping", function (req, res) {
     registroController.totalVisitantes(req, res);
 });
 
-router.get("/picoVisitantes/:fkShopping"), function (req, res) {
+router.get("/picoVisitantes/:fkShopping", function (req, res) {
     registroController.picoVisitantes(req, res);
-}
+});
 
-router.get("/mediaVisitantes/:fkShopping"), function (req, res) {
+router.get("/mediaVisitantes/:fkShopping", function (req, res) {
     registroController.mediaVisitantes(req, res);
-}
+});
 
 module.exports = router;
